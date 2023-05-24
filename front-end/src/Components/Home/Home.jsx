a1e87a99f13c169959ca4f650fff8e29e037c1c9
IFiTechnology <infinity3nity@gmail.com>
2023-05-24 14:58:28 +0100

    Made few changes

--- front-end/src/Components/Home/Home.jsx
+++ front-end/src/Components/Home/Home.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import video from "../../Assets/JWVideo.mp4";
 import "./Home.css";
 
 const Home = () => {
-  return <div>
-    <h1>House</h1>
-  </div>;
+  return (
+    <section className="home">
+      <div className="overlay"></div>
+
+      <video src={video} muted autoPlay loop type="video/mp4"></video>
+
+      <div className="homeContent container">
+        <div className="textDiv">
+          <h1 className="homeTile">
+          Inspiring intimacy with GOD through Worship and Praise
+          </h1>
+          <p className="smallText">
+          Amongst the thousand let your voice be heard.
+          </p>
+        </div>
+        <div className="buttonDiv">
+          <button className="btn join">Join US</button>
+          <button className="btn live">LIVE STREAM</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
--- front-end/src/Components/Main/Main.jsx
+++ front-end/src/Components/Main/Main.jsx
@@ -1,8 +1,79 @@
 import React from "react";
+import Wgathering from "../../Assets/WGathering.jpg";
+import JWUnec from "../../Assets/JWUnec.jpg";
+import JWAbuja from "../../Assets/JWAbuja.jpg";
+import LiveRecording from "../../Assets/LiveRecording.jpg";
 import "./Main.css";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div className="main">
+      <section className="event">
+        <h1 className="sectionTitle">Upcoming Events</h1>
+        <div class="events">
+          <div class="eventCard">
+            <div class="eventCard-image">
+              <img src={Wgathering} />
+            </div>
+            <div class="eventCard-text">
+              <h2 class="eventCard-title">Worshipers Gathering</h2>
+              <p class="eventCard-body">
+                Worshipper gathering is our weekly worship school, every Sunday
+                by 4pm where we equip, train and raise, genuine worshippers of
+                Jesus Christ.
+              </p>
+            </div>
+            <div class="eventCard-date">Every Mon 5PM</div>
+          </div>
+          <div class="eventCard">
+            <div class="eventCard-image">
+              <img src={JWUnec} />
+            </div>
+            <div class="eventCard-text">
+              <h2 class="eventCard-title">Just Worship Unec</h2>
+              <p class="eventCard-body">
+                The Lord is calling us back to campus outreaches as the ministry
+                expands to different cities and nations. We are starting the
+                campus tour from the University of Nigeria Enugu Campus (UNEC).
+              </p>
+            </div>
+            <div class="eventCard-date">Every Mon 5PM</div>
+          </div>
+          <div class="eventCard">
+            <div class="eventCard-image">
+              <img src={JWAbuja} />
+            </div>
+            <div class="eventCard-text">
+              <h2 class="eventCard-title">Just Worship Abuja</h2>
+              <p class="eventCard-body">
+                As the Vision keeps growing Just Worship Team is delighted hold
+                her first evening of worship in the city of Abuja.
+              </p>
+            </div>
+            <div class="eventCard-date">Every Mon 5PM</div>
+          </div>
+          <div class="eventCard">
+            <div class="eventCard-image">
+              <img src={LiveRecording} />
+            </div>
+            <div class="eventCard-text">
+              <h2 class="eventCard-title">Worshipers Gathering</h2>
+              <p class="eventCard-body">
+                Worshipper gathering is our weekly worship school, every Sunday
+                by 4pm where we equip, train and raise, genuine worshippers of
+                Jesus Christ.
+              </p>
+            </div>
+            <div class="eventCard-date">Every Mon 5PM</div>
+          </div>
+        </div>
+      </section>
+
+      <section className="leadership">
+        <h1 className="sectionTitle">Our Leadership</h1>
+      </section>
+    </div>
+  );
 };
 
 export default Main;
