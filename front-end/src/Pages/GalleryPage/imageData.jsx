b111a8a93b15c8be68c1af1f89762df610eff917
IFiTechnology <104058999+IFiTechnology@users.noreply.github.com>
2023-07-16 15:35:59 +0100

    Gallery Page

--- front-end/src/Pages/GalleryPage/imageData.jsx
+++ front-end/src/Pages/GalleryPage/imageData.jsx
@@ -1,29 +1,52 @@
 // imageData.j
 
-import image1 from '../../Assets/outreach/sixteen/2016image1.JPG';
-import image2 from '../../Assets/outreach/sixteen/2016image2.JPG';
-import image3 from '../../Assets/outreach/sixteen/2016image3.JPG';
-// import image4 from '../../Assets/outreach/sixteen/2016image4.JPG';
-import image5 from '../../Assets/outreach/sixteen/2016image5.JPG';
-import image6 from '../../Assets/outreach/sixteen/2016image6.JPG';
-import image7 from '../../Assets/outreach/sixteen/2016image7.JPG';
-import image8 from '../../Assets/outreach/sixteen/2016image8.JPG';
-import image9 from '../../Assets/outreach/sixteen/2016image9.JPG';
-// import image10 from '../../Assets/outreach/twentyTwo/2022image10.JPG';
-// import image11 from '../../Assets/outreach/twentyTwo/2022image11.JPG';
-// import image13 from '../../Assets/outreach/twentyTwo/2022image12.JPG';
-// import image14 from '../../Assets/outreach/twentyTwo/2022image13.JPG';
-// import image15 from '../../Assets/outreach/twentyTwo/2022image14.JPG';
-// import image16 from '../../Assets/outreach/twentyTwo/2022image15.JPG';
-// import image17 from '../../Assets/outreach/twentyTwo/2022image16.JPG';
-// import image12 from '../../Assets/outreach/twentyTwo/2022image17.JPG';
-// import image18 from '../../Assets/outreach/twentyTwo/2022image18.JPG';
-// import image19 from '../../Assets/outreach/twentyTwo/2022image19.JPG';
-// import image20 from '../../Assets/outreach/twentyTwo/2022image20.JPG';
-// import image21 from '../../Assets/outreach/twentyTwo/2022image21.JPG';
-// import image22 from '../../Assets/outreach/twentyTwo/2022image22.JPG';
-// import image23 from '../../Assets/outreach/twentyTwo/2022image23.JPG';
-// import image24 from '../../Assets/outreach/twentyTwo/2022image24.JPG';
+import image1 from '../../Assets/outreach/2016image1.JPG';
+import image2 from '../../Assets/outreach/2016image2.JPG';
+import image3 from '../../Assets/outreach/2016image3.JPG';
+// import image4 from '../../Assets/outrheen/2016image4.JPG';
+import image5 from '../../Assets/outreach/2016image5.JPG';
+import image6 from '../../Assets/outreach/2016image6.JPG';
+import image7 from '../../Assets/outreach/2016image7.JPG';
+import image8 from '../../Assets/outreach/2016image8.JPG';
+import image9 from '../../Assets/outreach/2016image9.JPG';
+// import image10 from '../../Assets/outreach/22image1.JPG';
+// import image11 from '../../Assets/outreach/22image2.JPG';
+import image13 from '../../Assets/campusTour/17image1.JPG';
+import image14 from '../../Assets/campusTour/17image2.JPG';
+import image15 from '../../Assets/campusTour/17image3.JPG';
+import image16 from '../../Assets/campusTour/17image4.JPG';
+import image17 from '../../Assets/campusTour/17image5.JPG';
+import image12 from '../../Assets/campusTour/17image6.JPG';
+import image18 from '../../Assets/campusTour/17image7.JPG';
+import image19 from '../../Assets/campusTour/17image8.JPG';
+import image20 from '../../Assets/campusTour/17image9.JPG';
+import image21 from '../../Assets/campusTour/17image10.JPG';
+import image22 from '../../Assets/campusTour/17image11.JPG';
+import image23 from '../../Assets/campusTour/17image12.JPG';
+import image24 from '../../Assets/campusTour/17image13.JPG';
+import image25 from '../../Assets/campusTour/17image14.JPG';
+import image26 from '../../Assets/campusTour/17image15.JPG';
+import image27 from '../../Assets/campusTour/17image16.JPG';
+import image28 from '../../Assets/campusTour/17image17.JPG';
+import image29 from '../../Assets/campusTour/17image18.JPG';
+import image30 from '../../Assets/campusTour/17image19.JPG';
+import image31 from '../../Assets/campusTour/17image20.JPG';
+import image32 from '../../Assets/campusTour/17image21.JPG';
+import image33 from '../../Assets/campusTour/17image22.JPG';
+import image34 from '../../Assets/campusTour/17image23.JPG';
+import image35 from '../../Assets/campusTour/17image24.JPG';
+import image36 from '../../Assets/campusTour/17image25.JPG';
+import image37 from '../../Assets/campusTour/17image26.JPG';
+import image38 from '../../Assets/campusTour/17image14.JPG';
+import image39 from '../../Assets/campusTour/17image15.JPG';
+import image40 from '../../Assets/campusTour/17image16.JPG';
+import image41 from '../../Assets/campusTour/17image17.JPG';
+import image42 from '../../Assets/campusTour/17image18.JPG';
+import image43 from '../../Assets/campusTour/17image19.JPG';
+import image44 from '../../Assets/campusTour/17image20.JPG';
+import image45 from '../../Assets/campusTour/17image21.JPG';
+import image46 from '../../Assets/campusTour/17image22.JPG';
+
 
 
 
@@ -39,20 +62,42 @@ export const imageData = [
   { category: 'Outreach', url: image9 },
   // { category: 'Outreach', url: image10 },
   // { category: 'Outreach', url: image11 },
-  // { category: 'Outreach', url: image12},
-  // { category: 'Outreach', url: image13 },
-  // { category: 'Outreach', url: image14 },
-  // { category: 'Outreach', url: image15 },
-  // { category: 'Outreach', url: image16 },
-  // { category: 'Outreach', url: image17 },
-  // { category: 'Outreach', url: image18 },
-  // { category: 'Outreach', url: image19 },
-  // { category: 'Outreach', url: image20 },
-  // { category: 'Outreach', url: image21 },
-  // { category: 'Outreach', url: image22 },
-  // { category: 'Outreach', url: image23 },
-  // { category: 'Outreach', url: image24},
-  // Add more image data here
+  { category: 'Campus Tour', url: image12 },
+  { category: 'Campus Tour', url: image13 },
+  { category: 'Campus Tour', url: image14 },
+  { category: 'Campus Tour', url: image15 },
+  { category: 'Campus Tour', url: image16 },
+  { category: 'Campus Tour', url: image17 },
+  { category: 'Campus Tour', url: image18 },
+  { category: 'Campus Tour', url: image19 },
+  { category: 'Campus Tour', url: image20 },
+  { category: 'Campus Tour', url: image21 },
+  { category: 'Campus Tour', url: image22 },
+  { category: 'Campus Tour', url: image23 },
+  { category: 'Campus Tour', url: image24 },
+  { category: 'Campus Tour', url: image25 },
+  { category: 'Campus Tour', url: image26 },
+  { category: 'Campus Tour', url: image27 },
+  { category: 'Campus Tour', url: image28 },
+  { category: 'Campus Tour', url: image29 },
+  { category: 'Campus Tour', url: image30 },
+  { category: 'Campus Tour', url: image31 },
+  { category: 'Campus Tour', url: image32 },
+  { category: 'Campus Tour', url: image33 },
+  { category: 'Campus Tour', url: image34 },
+  { category: 'Campus Tour', url: image35 },
+  { category: 'Campus Tour', url: image36 },
+  { category: 'Campus Tour', url: image37 },
+  { category: 'Campus Tour', url: image38 },
+  { category: 'Campus Tour', url: image39 },
+  { category: 'Campus Tour', url: image40 },
+  { category: 'Campus Tour', url: image41 },
+  { category: 'Campus Tour', url: image42 },
+  { category: 'Campus Tour', url: image43 },
+  { category: 'Campus Tour', url: image44 },
+  { category: 'Campus Tour', url: image45 },
+  { category: 'Campus Tour', url: image46 },
+  
 ];
 
 
